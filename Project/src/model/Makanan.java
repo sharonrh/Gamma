ed6fd59bedb27bed6dfe052f330dd3e56e4afac5
fajarsiddiq <fajar.manutd@gmail.com>
2014-04-06 17:43:00 +0700

    Editing atribut Makanan

--- Project/src/model/Makanan.java
+++ Project/src/model/Makanan.java
@@ -3,20 +3,26 @@ package model;
 public class Makanan {
 
 	private String nama;
-	private int kalori;
-	private int protein;
-	private int karbohidrat;
-	private int lemak;
+	private double kalori;
+	private double protein;
+	private double karbohidrat;
+	private double lemak;
+	private double kalsium;
+	private int persentase;
+	private int rating;
 	private String jenisMakanan;
 	
-	public Makanan(String nama, int kalori, int protein, int karbohidrat,
-			int lemak, String jenisMakanan) {
+	public Makanan(String nama, double kalori, double protein, double karbohidrat,
+			double lemak, double kalsium, int persentase, int rating, String jenisMakanan) {
 		super();
 		this.nama = nama;
 		this.kalori = kalori;
 		this.protein = protein;
 		this.karbohidrat = karbohidrat;
 		this.lemak = lemak;
+		this.kalsium = kalsium;
+		this.persentase = persentase;
+		this.rating = rating;
 		this.jenisMakanan = jenisMakanan;
 	}
 
@@ -28,36 +34,60 @@ public class Makanan {
 		this.nama = nama;
 	}
 
-	public int getKalori() {
+	public double getKalori() {
 		return kalori;
 	}
 
-	public void setKalori(int kalori) {
+	public void setKalori(double kalori) {
 		this.kalori = kalori;
 	}
 
-	public int getProtein() {
+	public double getProtein() {
 		return protein;
 	}
 
-	public void setProtein(int protein) {
+	public void setProtein(double protein) {
 		this.protein = protein;
 	}
 
-	public int getKarbohidrat() {
+	public double getKarbohidrat() {
 		return karbohidrat;
 	}
 
-	public void setKarbohidrat(int karbohidrat) {
+	public void setKarbohidrat(double karbohidrat) {
 		this.karbohidrat = karbohidrat;
 	}
 
-	public int getLemak() {
+	public double getLemak() {
 		return lemak;
 	}
 
-	public void setLemak(int lemak) {
+	public void setLemak(double lemak) {
 		this.lemak = lemak;
+	}
+
+	public double getKalsium() {
+		return kalsium;
+	}
+
+	public void setKalsium(double kalsium) {
+		this.kalsium = kalsium;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getPersentase() {
+		return persentase;
+	}
+
+	public void setPersentase(int persentase) {
+		this.persentase = persentase;
 	}
 
 	public String getJenisMakanan() {
