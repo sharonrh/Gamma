d302cda4160a7105f668e8099ed4679f8fb1aa75
retsiem <bravesperia@hotmail.com>
2014-04-12 10:36:23 +0700

    Lihat Profil & Edit Profil
    Baru XMLnya, fungsi javanya belum

--- Project/src/com/example/gamma/EditProfilActivity.java
+++ Project/src/com/example/gamma/EditProfilActivity.java
@@ -10,10 +10,53 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class EditProfilActivity extends Activity {
 
+	Spinner spinTransport;
+	
 	@Override
+	 public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.activity_edit_profil);
+	 
+	  spinTransport = (Spinner) findViewById(R.id.spinnerGayaHidup);
+	   
+	  // Adapter for spinner
+	        ArrayAdapter adapter = ArrayAdapter.createFromResource(getApplicationContext(),  R.array.isi_gayaHidup, android.R.layout.simple_spinner_dropdown_item);
+	        // Sets the layout resource to create the drop down views
+	        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	  
+	        //The Adapter is used to provide the data which backs this Spinner.
+	        spinTransport.setAdapter(adapter);
+	        spinTransport.setOnItemSelectedListener(new OnItemSelectedListener(){
+	         public void onItemSelected(AdapterView parent, View view, int position,
+	           long id) {
+	 
+	          // On selecting a spinner item
+	          String item = parent.getItemAtPosition(position).toString();
+	 
+	          // showing a toast on selecting an item
+	          Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
+	 
+	         }
+	 
+	   public void onNothingSelected(AdapterView arg0) {
+	    // TODO Auto-generated method stub
+	     
+	   }
+	        });
+	 }
+	 
+	 
+	
+	
+	/**@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_profil);
@@ -42,12 +85,12 @@ public class EditProfilActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
+	}*/
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	/**public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
@@ -59,6 +102,6 @@ public class EditProfilActivity extends Activity {
 					container, false);
 			return rootView;
 		}
-	}
+	}*/
 
 }
