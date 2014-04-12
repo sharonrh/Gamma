d4579c0858a68ca1ad46012f0d4b0983d15fd94f
retsiem <bravesperia@hotmail.com>
2014-04-12 14:08:51 +0700

    coba

--- Project/src/com/example/gamma/EditProfilActivity.java
+++ Project/src/com/example/gamma/EditProfilActivity.java
@@ -20,6 +20,9 @@ import android.widget.AdapterView.OnItemSelectedListener;
 public class EditProfilActivity extends Activity {
 
 	Spinner spinTransport;
+	Spinner spinTransport2;
+	Spinner spinTransport3;
+	Spinner spinTransport4;
 	
 	@Override
 	 public void onCreate(Bundle savedInstanceState) {
@@ -27,14 +30,23 @@ public class EditProfilActivity extends Activity {
 	  setContentView(R.layout.activity_edit_profil);
 	 
 	  spinTransport = (Spinner) findViewById(R.id.spinnerGayaHidup);
+	  spinTransport2 = (Spinner) findViewById(R.id.spinnerAktivitas1);
+	  spinTransport3 = (Spinner) findViewById(R.id.spinnerAktivitas2);
+	  spinTransport4 = (Spinner) findViewById(R.id.spinnerAktivitas3);
 	   
 	  // Adapter for spinner
 	        ArrayAdapter adapter = ArrayAdapter.createFromResource(getApplicationContext(),  R.array.isi_gayaHidup, android.R.layout.simple_spinner_dropdown_item);
+	        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(getApplicationContext(),  R.array.isi_aktivitas, android.R.layout.simple_spinner_dropdown_item);
 	        // Sets the layout resource to create the drop down views
 	        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	  
+	        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	        
 	        //The Adapter is used to provide the data which backs this Spinner.
 	        spinTransport.setAdapter(adapter);
+	        spinTransport2.setAdapter(adapter2);
+	        spinTransport3.setAdapter(adapter2);
+	        spinTransport4.setAdapter(adapter2);
+	        
 	        spinTransport.setOnItemSelectedListener(new OnItemSelectedListener(){
 	         public void onItemSelected(AdapterView parent, View view, int position,
 	           long id) {
@@ -46,13 +58,77 @@ public class EditProfilActivity extends Activity {
 	          Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
 	 
 	         }
+	         
+	         
 	 
 	   public void onNothingSelected(AdapterView arg0) {
 	    // TODO Auto-generated method stub
 	     
 	   }
 	        });
-	}}
+	        
+	        spinTransport2.setOnItemSelectedListener(new OnItemSelectedListener(){
+		         public void onItemSelected(AdapterView parent, View view, int position,
+		           long id) {
+		 
+		          // On selecting a spinner item
+		          String item = parent.getItemAtPosition(position).toString();
+		 
+		          // showing a toast on selecting an item
+		          Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
+		 
+		         }
+		         
+		         
+		 
+		   public void onNothingSelected(AdapterView arg0) {
+		    // TODO Auto-generated method stub
+		     
+		   }
+		        });
+	        
+	        spinTransport3.setOnItemSelectedListener(new OnItemSelectedListener(){
+		         public void onItemSelected(AdapterView parent, View view, int position,
+		           long id) {
+		 
+		          // On selecting a spinner item
+		          String item = parent.getItemAtPosition(position).toString();
+		 
+		          // showing a toast on selecting an item
+		          Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
+		 
+		         }
+		         
+		         
+		 
+		   public void onNothingSelected(AdapterView arg0) {
+		    // TODO Auto-generated method stub
+		     
+		   }
+		        });
+	        
+	        spinTransport4.setOnItemSelectedListener(new OnItemSelectedListener(){
+		         public void onItemSelected(AdapterView parent, View view, int position,
+		           long id) {
+		 
+		          // On selecting a spinner item
+		          String item = parent.getItemAtPosition(position).toString();
+		 
+		          // showing a toast on selecting an item
+		          Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
+		 
+		         }
+		         
+		         
+		 
+		   public void onNothingSelected(AdapterView arg0) {
+		    // TODO Auto-generated method stub
+		     
+		   }
+		        });
+	        
+	}
+}
 	 
 	
 	
