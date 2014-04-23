b0191f1e1216206f9a9fd4e0a52c977a66c89390
sharonrh <frozen_flame46@yahoo.com>
2014-04-24 04:13:57 +0700

    Tab ganti navigation drawer : done

--- Project/src/view/MainActivity.java
+++ Project/src/view/MainActivity.java
@@ -1,22 +1,39 @@
 package view;
 
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.app.FragmentManager;
 import android.content.Context;
-import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.gamma.R;
 
 public class MainActivity extends Activity {
 
-	private static final String TAB_KEY_INDEX = "tab_key";
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	private String[] mNavTitles;
+	private TypedArray mNavIcons;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -24,45 +41,58 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Utils.setThemeToActivity(this);
-		// ActionBar
-		ActionBar actionbar = getActionBar();
-		actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		// create new tabs and and set up the titles of the tabs
-		ActionBar.Tab rekTab = actionbar.newTab().setText(
-				getString(R.string.ui_tabname_rekomendasi));
-		ActionBar.Tab laporanTab = actionbar.newTab().setText(
-				getString(R.string.ui_tabname_laporan));
-		ActionBar.Tab profilTab = actionbar.newTab().setText(
-				getString(R.string.ui_tabname_profil));
-		ActionBar.Tab statTab = actionbar.newTab().setText(
-				getString(R.string.ui_tabname_statistik));
+		mTitle = mDrawerTitle = getTitle();
+		mNavTitles = getResources().getStringArray(R.array.nav_drawer_items);
+		mNavIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-		Fragment rekFragmen = new RekomendasiFragment();
-		Fragment laporanFragment = new LaporanFragment();
-		Fragment profilFragment = new ProfilFragment();
-		Fragment statFragment = new StatistikFragment();
+		// set a custom shadow that overlays the main content when the drawer
+		// opens
+		// mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+		// GravityCompat.START);
+		// set up the drawer's list view with items and click listener
+		int size = mNavTitles.length;
+		DrawerItem[] items = new DrawerItem[size];
 
-		rekTab.setTabListener(new MyTabsListener(rekFragmen));
-		laporanTab.setTabListener(new MyTabsListener(laporanFragment));
-		profilTab.setTabListener(new MyTabsListener(profilFragment));
-		statTab.setTabListener(new MyTabsListener(statFragment));
-
-		actionbar.addTab(rekTab);
-		actionbar.addTab(laporanTab);
-		actionbar.addTab(statTab);
-		actionbar.addTab(profilTab);
-
-		// add the tabs to the action bar
-		Intent i = getIntent();
-		int x = i.getIntExtra("nomorFragment", 0);
-		actionbar.setSelectedNavigationItem(x);
-
-		// restore to navigation
-		if (savedInstanceState != null) {
-			actionbar.setSelectedNavigationItem(savedInstanceState.getInt(
-					TAB_KEY_INDEX, 0));
+		for (int i = 0; i < size; i++) {
+			items[i] = new DrawerItem(mNavTitles[i], mNavIcons.getResourceId(i, -1));
 		}
+
+		mDrawerList.setAdapter(new NavDrawerListAdapter(this, items));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description for accessibility */
+		R.string.drawer_close /* "close drawer" description for accessibility */
+		) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		if (savedInstanceState == null) {
+			selectItem(0);
+		}
+
 	}
 
 	@Override
@@ -72,55 +102,170 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	/**
-	 * Event Handling for Individual menu item selected Identify single menu
-	 * item by it's id
-	 * */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action buttons
+		return super.onOptionsItemSelected(item);
+	}
 
-		switch (item.getItemId()) {
-		case R.id.action_settings:
-			Intent i = new Intent(this, SettingActivity.class);
-			startActivity(i);
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
 
-		default:
-			return super.onOptionsItemSelected(item);
+	/* The click listner for ListView in the navigation drawer */
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItem(position);
 		}
 	}
 
-	// onSaveInstanceState() is used to "remember" the current state when a
-	// configuration change occurs such screen orientation change. This
-	// is not meant for "long term persistence". We store the tab navigation
+	private void selectItem(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = null;
+		switch (position) {
+
+		case 0:
+			fragment = new RekomendasiFragment();
+			break;
+		case 1:
+			fragment = new ProfilFragment();
+			break;
+		case 2:
+			fragment = new StatistikFragment();
+			break;
+		case 3:
+			fragment = new LaporanFragment();
+			break;
+		case 4:
+			fragment = new LaporanFragment(); // ntar ganti katalog
+			break;
+		case 5:
+			fragment = new LaporanFragment(); // ntar ganti achievement
+			break;
+		case 6:
+			fragment = new LaporanFragment(); // ntar ganti setting
+			break;
+		default:
+			break;
+		}
+		if (fragment != null) {
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.beginTransaction()
+					.replace(R.id.content_frame, fragment).commit();
+
+			// update selected item and title, then close the drawer
+			mDrawerList.setItemChecked(position, true);
+			setTitle(mNavTitles[position]);
+			mDrawerLayout.closeDrawer(mDrawerList);
+		} else {
+			// error in creating fragment
+			Log.e("MainActivity", "Error in creating fragment");
+		}
+	}
 
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putInt(TAB_KEY_INDEX, getActionBar()
-				.getSelectedNavigationIndex());
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
 	}
 
-	class MyTabsListener implements ActionBar.TabListener {
-		public Fragment fragment;
-		public Context context;
+	/**
+	 * When using the ActionBarDrawerToggle, you must call it during
+	 * onPostCreate() and onConfigurationChanged()...
+	 */
 
-		public MyTabsListener(Fragment fragment) {
-			this.fragment = fragment;
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+}
+
+class NavDrawerListAdapter extends ArrayAdapter<DrawerItem> {
+
+	private Context context;
+	private DrawerItem[] items;
+
+	static class ViewHolder {
+		public ImageView icon;
+		public TextView title;
+	}
+
+	public NavDrawerListAdapter(Activity context, DrawerItem[] items) {
+		super(context, R.layout.drawer_list_item, items);
+		this.context = context;
+		this.items = items;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+		if (rowView == null) {
+			LayoutInflater mInflater = (LayoutInflater) context
+					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			rowView = mInflater.inflate(R.layout.drawer_list_item, null);
+
+			ViewHolder viewHolder = new ViewHolder();
+			viewHolder.icon = (ImageView) rowView.findViewById(R.id.icon);
+			viewHolder.title = (TextView) rowView.findViewById(R.id.title);
+			rowView.setTag(viewHolder);
 		}
 
-		@Override
-		public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		}
+		ViewHolder holder = (ViewHolder) rowView.getTag();
 
-		@Override
-		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-			ft.replace(R.id.fragment_container, fragment);
-		}
+		holder.icon.setImageResource(items[position].getIcon());
+		holder.title.setText(items[position].getTitle());
 
-		@Override
-		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-			ft.remove(fragment);
-		}
+		return rowView;
+	}
+}
 
+class DrawerItem {
+	public String title;
+	public int icon;
+
+	public DrawerItem(String title, int icon) {
+		super();
+		this.title = title;
+		this.icon = icon;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getIcon() {
+		return icon;
+	}
+
+	public void setIcon(int icon) {
+		this.icon = icon;
 	}
 }
